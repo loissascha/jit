45b5973963e002dab78ae782a0a6e725ab80b7b0
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-31 18:53:06 +0100

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,23 @@ import (
 	"path/filepath"
 )
 
+type Config struct {
+	Username string            `json:"username"`
+	Remote   Remote            `json:"remote"`
+	Branches map[string]Branch `json:"branches"`
+}
+
+type Remote struct {
+	Url string `json:"url"`
+}
+
+type Branch struct {
+}
+
+func LoadMainConfigFile(basepath string) *Config {
+	return &Config{}
+}
+
 func GetMainConfigFilePath(basepath string) string {
 	return filepath.Join(basepath, ".jit/config")
 }
